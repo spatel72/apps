022e51ad53c1400dc6702e8ba4a65985afb7641b
modelearth <model@dreamstudio.com>
2021-08-22 11:59:26 -0400

    Add base starter folder

--- apps.md
+++ apps.md
@@ -1,6 +1,7 @@
-[Brigade Impacts](brigades/)  
-[Beyond Carbon](beyondcarbon/)  
-[Coastal Recycling](coastal/)  
-[EV Manufacturing](ev/)  
+[Base](base/) - Starter folder  
+[Beyond Carbon](beyondcarbon/) - Combing Bloomberg and EPA data  
+[Brigade Map](brigades/) - Combing Code for America and EPA data  
+[Coastal Recycling](coastal/) - Southeast Georgia Team  
+[EV Manufacturing](ev/) - LaGrange Georgia Team  
 
-[Make Updates](https://github.com/modelearth/apps/)
+[Fork from GitHub](https://github.com/modelearth/apps/) - Click "Fork" in the upper right - [Local site setup](../localsite/start/)  
---
+++ js/bc.js
@@ -0,0 +1,86 @@
+//loadMarkdown("../../community/challenge/README.md", "readmeDiv", "_parent");
+
+// Within localhost.js the state menu is moved into the hero image area by the following:
+// relocatedStateMenu.appendChild(state_select);
+
+if(typeof param == 'undefined') {
+  var param = {};
+}
+
+$(document).ready(function() {
+
+  // `hashChangeEvent` event reside in multiple widgets. Also called by involking goHash() within localsite.js
+  document.addEventListener('hashChangeEvent', function (elem) {
+      //let hash = getHash();
+
+      //let theState = $("#state_select").find(":selected").val();
+      let theStateName = $("#state_select").find(":selected").text();
+      //let theStateNameLowercase = theStateName.toLowerCase();
+
+      console.log("theStateName from hashChangeEvent: " + theStateName);
+      displayStateData(theStateName);
+  }, false);
+});
+
+var stateDataset = [];
+function loadHtmlTable(applyFilter) {
+  //alert("loading")
+  d3.text("5_22-data-06_06.csv").then(function(data) {
+      stateDataset = d3.csvParseRows(data);
+      console.log("loadHtmlTable - stateDataset row count: " + stateDataset.length);
+      //alert(stateDataset);
+
+        let theStateName = $("#state_select").find(":selected").text();
+        //alert("theStateName " + theStateName);
+        setTimeout( function() {
+          theStateName = $("#state_select").find(":selected").text();
+          if(!theStateName) { // Hack. We need to instead trigger when #state_select menu becomes available.
+            //theStateName = "Georgia"
+          }
+          displayStateData(theStateName);
+        }, 1000 ); // Allow time for state dropdown to load.
+  });
+}
+
+//setTimeout( function() {
+  loadHtmlTable(true); // New list
+//}, 1000 ); // Allow time for state dropdown to load.
+
+function statePhrase(stateRow, rowIndex, theStateName) {
+  return(stateRow[rowIndex].replace("[XX]" || "[XX's]", theStateName) )
+}
+function displayStateData(theStateName) {
+  console.log("displayStateData: " + theStateName);
+  if (theStateName.length == 0) {
+    //alert("test")
+    $("#about-profile").show();
+    $("#choose-counties").hide();
+    $("#dataDisplay").hide();
+    return;
+  }
+  $("#about-profile").hide();
+  $("#choose-counties").show();
+  $("#dataDisplay").show();
+
+  let rowcount = 0;
+  let dataRow = "";
+  
+  for(var i = 0; i < stateDataset.length; i++) {
+    rowcount++;
+    if (stateDataset[i][0]==theStateName) {
+      
+      dataRow += "<table id='resultsTable'>";
+      dataRow += "<tr><td>" + statePhrase(stateDataset[i], 5, theStateName) + "</td></tr>"
+      dataRow += "<tr><td>Has " + theStateName + " committed to 100% clean energy? " + statePhrase(stateDataset[i], 1, theStateName) + "</td></tr>"
+      dataRow += "<tr><td>" + statePhrase(stateDataset[i], 2, theStateName)  + "</td></tr>"
+      dataRow += "<tr><td>Does " + theStateName + " have a goal for reducing carbon pollution across the entire economy? " + statePhrase(stateDataset[i], 3, theStateName)  + "</td></tr>"
+      dataRow += "<tr><td>Does " + theStateName + " have goals or incentives for electric vehicles? " + statePhrase(stateDataset[i], 4, theStateName) + "</td></tr>"
+      dataRow += "</table>";
+    }
+  }
+  //alert("rowcount " + rowcount);
+  //$(dataRow).insertAfter($("#dataHeader"));
+
+  $("#dataDisplay").html(dataRow);
+  //$("#dataHeader").html(dataRow);
+}
